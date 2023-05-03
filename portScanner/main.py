c78eeeb274a004060b478c92a5de0245e55ec365
kirestein <erik.proenca2011@gmail.com>
2023-05-03 18:08:30 -0300

    improuving the code

--- portScanner/main.py
+++ portScanner/main.py
@@ -1,14 +1,25 @@
 import socket
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # IPV4 e TCP IP
-client.settimeout(0.05)
-
 ip = input('Enter the host or IP to be verify: ')
-port = int(input('Enter the port to be verify: '))
 
-code = client.connect_ex((ip, port))
+ports = []
+count = 0
 
-if code == 0:
-    print('Port is open')
-else:
-    print('Port is closed')
+while count < 10:
+    ports.append(int(input('Enter the port to be verify: ')))
+    count += 1
+print('')
+print('---------------OUTPUT---------------')
+print('')
+for port in ports:
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # IPV4 e TCP IP
+    client.settimeout(0.05)
+    code = client.connect_ex((ip, port))
+
+    if code == 0:
+        print(f'Port {port} is open')
+    else:
+        print(f'Port {port} is closed')
+
+print('')
+print('Scan finished!')

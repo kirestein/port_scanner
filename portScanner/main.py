3f1b77e384a19b700f774690120b6a270d4b9911
kirestein <erik.proenca2011@gmail.com>
2023-05-03 17:43:07 -0300

    conditional to verify if port is open or closed

--- portScanner/main.py
+++ portScanner/main.py
@@ -1,7 +1,14 @@
 import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # IPV4 e TCP IP
+client.settimeout(0.05)
 
-code = client.connect_ex(('google.com', 80))
+ip = input('Enter the host or IP to be verify: ')
+port = int(input('Enter the port to be verify: '))
 
-print(f'{code=}')
+code = client.connect_ex((ip, port))
+
+if code == 0:
+    print('Port is open')
+else:
+    print('Port is closed')
